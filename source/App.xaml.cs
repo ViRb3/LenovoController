1d59a7eab7e90dc6eb2c3497a5ff6b56920e81d2
Victor <virb3e@gmail.com>
2020-12-22 23:37:56 +0000

    fix: print exceptions if occurred

--- source/App.xaml.cs
+++ source/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace LenovoController
 {
@@ -7,5 +10,12 @@ namespace LenovoController
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var errorText = e.Exception.ToString();
+            Trace.TraceError(errorText);
+            MessageBox.Show(errorText, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Environment.Exit(-1);
+        }
     }
 }
